647d6b07d0f6bef3ce51856762c73b0dfd207462
Lesaje <olehchaika.prop@gmail.com>
2023-12-20 15:37:06 +0200

    first commit

---
+++ backend_server/utils.py
@@ -0,0 +1,16 @@
+# Copy and paste your OpenAI API Keypython manage.py runserver
+openai_api_key = "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"
+# Put your name
+key_owner = "Student"
+
+maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "../../environment/frontend_server/storage"
+fs_temp_storage = "../../environment/frontend_server/temp_storage"
+
+collision_block_id = "32125"
+
+# Verbose
+debug = True
